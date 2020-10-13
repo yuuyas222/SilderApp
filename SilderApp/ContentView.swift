7722b5009e414b3d6dfcbf4e804e09466c00a33e
佐藤　祐弥 <y.satou@satouyuuwatarunoMacBook-Air.local>
2020-10-13 17:54:24 +0900

    first commit

---
+++ SilderApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SilderApp
+//
+//  Created by 佐藤　祐弥 on 2020/10/13.
+//  Copyright © 2020 佐藤　祐弥. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
