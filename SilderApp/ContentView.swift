cad6a547a00abc61adf9238295d1215ec5e7ed75
佐藤　祐弥 <y.satou@satouyuuwatarunoMacBook-Air.local>
2020-10-13 18:22:54 +0900

    add slider

--- SilderApp/ContentView.swift
+++ SilderApp/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+            HStack {
+                Image(systemName:"speaker.fill")
+                Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/)
+                Image(systemName: "speaker.3.fill")
+            }
+            .accentColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
